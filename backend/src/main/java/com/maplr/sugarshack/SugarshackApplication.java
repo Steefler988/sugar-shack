2149ab7e18115d913bca2a9fe6b19f8419298efb
Steefler988 <raquin.philippe@gmail.com>
2023-03-14 16:53:43 +1100

    livraison initial backend

---
+++ backend/src/main/java/com/maplr/sugarshack/SugarshackApplication.java
@@ -0,0 +1,13 @@
+package com.maplr.sugarshack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SugarshackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SugarshackApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/maplr/sugarshack/SugarshackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maplr.sugarshack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SugarshackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
